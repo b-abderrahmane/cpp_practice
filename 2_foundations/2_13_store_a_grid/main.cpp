81302750d412cf794a370b7d4649b02d66e69f85
abdou <b-abderrahmane@outlook.com>
2020-05-03 17:32:48 +0200

    first exercise

---
+++ 2_foundations/2_13_store_a_grid/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<vector<int>> grid = {{0, 1, 0, 0, 0, 0},
+                              {0, 1, 0, 0, 0, 0},
+                              {0, 1, 0, 0, 0, 0},
+                              {0, 1, 0, 0, 0, 0},
+                              {0, 0, 0, 0, 1, 0}};
+}
